c18c28f0f76f1701b3045955536fc43e5d71736b
Jatin Aggarwal <135164893+Aggarwaljatin20@users.noreply.github.com>
2023-06-19 10:24:09 -0700

    Add files via upload

---
+++ modules/bootstrap.py
@@ -0,0 +1,30 @@
+import random
+import numpy as np
+def bootstrap(data,dl,dm):
+	N_1 = 250 # number used for training sample generation
+	N_2 = 150 # number used for testing sample generation
+	n_train = 250 # number of training samples to be generated
+	n_test = 150 # number of testing samples to be generated
+	data_train = np.zeros(n_train,2,640)
+	data_test = np.zeros(n_test,2,640)
+	idx = np.arange(0,N_1+N_2) # N_1 + N_2 is total number of the samples we have for each damage scenerio
+	random.shuffle(idx) # randomly shuffling the samples
+	idx_train = idx(0:N_1) #Diving the dataset into N_1 and N_2 dataset
+	idx_test = idx(N_1:end)
+	X=100 # number of samples used to calculate the average
+	DL  = dl[0] # damage location of the damage scenerio
+	DM  = dm[0] # damage magnitude of the damage scenerio	
+	for i in range(n_train):
+		random.shuffle(idx_train)
+		id = idx_train[0:X]
+		d = data[id,:,:]
+		d_avg = np.mean(d,axis=0)
+		data_train[i,:,:] = d_avg
+	for i in range(n_test):
+		random.shuffle(idx_test)
+		id = idx_test[0:X]
+		d = data[id,:,:]
+		d_avg = np.mean(d,axis=0)
+		data_test[i,:,:] = d_avg
+			
+return data_train,data_test,DL,DM
---
+++ modules/spatial_interpolation.py
@@ -0,0 +1,31 @@
+import os
+import glob
+from mat4py import loadmat
+import numpy as np
+import matplotlib.pyplot as plt
+
+def interpolation(f):
+  data = loadmat(f) #loading the matfiles from the folder
+  filename = data['Event']['file_name'] 
+  BL = int(filename[1:3]) #getting the bridge length from the file name
+  DM = int(filename[9:11])  # getting the damage magnitude (Dead Magnitude) from the file name
+  if DM ==0: # if undamaged, it is labeled as DL=0 (DL =  Dead Location)
+    DL=0
+  else: # if it is damaged, it is labeled as 1(DL=25) and 2 (DL=50)
+    DL = int(filename[5:7]) # getting the damage location from the file name
+  DM = int(DM/20) #0 (DM=00%), 1 (DM=20%), and 2 (DM=40%)
+  DL = int(DL/25) #0 (undamaged), 1 (DL=25%), and 2 (DL=50%)
+  a=data['Event']['Sol']['Veh']['A'] # Acceleration data of vehicle both body and axle
+  acc = np.array(a)
+  v = data['Event']['Veh']['Pos']['vel'] # velocity of the vehicle, required for spatial interpolation
+  dt = data['Event']['solver_dt'] # time step for the data collection
+  p1 = data['Event']['Veh']['Pos']['t0_ind_beam']-1 # location when it start reaches the bridge.
+  p2 = data['Event']['Veh']['Pos']['t_end_ind_beam'] # location when it reaches end of the bridge
+  acc = acc[:,p1:p2] # chopping the signal based on the location on the bridge
+  # frequency of sampling is 256Hz
+  x = v*1/256*np.linspace(p1,p2-1,p2-p1)-100 # obtaining spatial position of the signals
+  x_new = np.linspace(0,BL,640) # Equally spaced bridge locations
+  tmp = np.zeros((2,640))
+  for i in range(2):
+    inter_acc[i,:] = np.interp(x_new,x,acc[i,:]) # interpolated signals
+  return inter_acc,DM,DL
