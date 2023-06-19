d0a7a5bb128e8b55e1f6dce3f8db2f0e6bbaf9b8
Jatin Aggarwal <135164893+Aggarwaljatin20@users.noreply.github.com>
2023-06-19 12:12:43 -0700

    Update bootstrap.py

--- modules/bootstrap.py
+++ modules/bootstrap.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 def bootstrap(data,dl,dm):
+	
 	N_1 = 250 # number used for training sample generation
 	N_2 = 150 # number used for testing sample generation
 	n_train = 250 # number of training samples to be generated
@@ -9,8 +10,8 @@ def bootstrap(data,dl,dm):
 	data_test = np.zeros(n_test,2,640)
 	idx = np.arange(0,N_1+N_2) # N_1 + N_2 is total number of the samples we have for each damage scenerio
 	random.shuffle(idx) # randomly shuffling the samples
-	idx_train = idx(0:N_1) #Diving the dataset into N_1 and N_2 dataset
-	idx_test = idx(N_1:end)
+	idx_train = idx[0:N_1] #Dividing the dataset into N_1 and N_2 dataset
+	idx_test = idx[N_1:end]
 	X=100 # number of samples used to calculate the average
 	DL  = dl[0] # damage location of the damage scenerio
 	DM  = dm[0] # damage magnitude of the damage scenerio	
